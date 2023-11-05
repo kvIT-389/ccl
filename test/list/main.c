ea02021336a2ee791ee19e0a3fe5b1844112efea
kvIT-389 <vladimirkravcov389@gmail.com>
2023-11-06 00:36:41 +0800

    Add new list iterators test

--- test/list/main.c
+++ test/list/main.c
@@ -15,22 +15,44 @@ void f3() {
     printf("f3() call\n");
 }
 
-void call_from_list(list_t *list) {
-    list_iterator_t list_iter = list__get_iterator(list);
 
-    printf("Start iterating through list...\n");
+void iterate_forward(list_t *list) {
+    list_iterator_t it = list__begin(list);
 
-    void (*fn)() = list_iterator__current(&list_iter);
-    while (!list_iterator__ended(&list_iter)) {
+    printf("Start iterating forward through list...\n");
+
+    void (*fn)() = list_iterator__current(&it);
+    while (!list_iterator__ended(&it)) {
         /* Using value */
         if (fn != NULL) {
             fn();
         } else {
-            printf("Vector value is NULL.\n");
+            printf("List value is NULL.\n");
         }
 
         /* Updating value */
-        fn = list_iterator__next(&list_iter);
+        fn = list_iterator__next(&it);
+    }
+
+    printf("Iteration ended.\n");
+}
+
+void iterate_backward(list_t *list) {
+    list_iterator_t it = list__rbegin(list);
+
+    printf("Start iterating backward through list...\n");
+
+    void (*fn)() = list_iterator__current(&it);
+    while (!list_iterator__rended(&it)) {
+        /* Using value */
+        if (fn != NULL) {
+            fn();
+        } else {
+            printf("List value is NULL.\n");
+        }
+
+        /* Updating value */
+        fn = list_iterator__prev(&it);
     }
 
     printf("Iteration ended.\n");
@@ -40,15 +62,15 @@ void call_from_list(list_t *list) {
 int main(int argc, char const *argv[]) {
     list_t *list = list__create();
 
-    printf("Initial size: %d\n", list->size);
+    printf("Initial size: %d\n", list__size(list));
 
     list__push_back(list, &f1);
     list__push_back(list, &f3);
     list__push_front(list, &f2);
 
-    printf("Size after 3 pushes: %d\n", list->size);
+    printf("Size after 3 pushes: %d\n", list__size(list));
 
-    call_from_list(list);
+    iterate_forward(list);
 
     list__pop_back(list);
     list__pop_front(list);
@@ -57,17 +79,17 @@ int main(int argc, char const *argv[]) {
     list__push_front(list, &f3);
     list__push_front(list, &f2);
 
-    printf("Size after 2 pops and 3 pushes: %d\n", list->size);
+    printf("Size after 2 pops and 3 pushes: %d\n", list__size(list));
 
-    call_from_list(list);
+    iterate_backward(list);
 
     list__clear(list);
 
-    printf("Size after clear(): %d\n", list->size);
+    printf("Size after clear(): %d\n", list__size(list));
 
     list__free(list);
 
-    printf("Size after free(): %d\n (undefined)", list->size);
+    printf("Size after free(): %d [undefined]\n", list__size(list));
 
     return 0;
 }
